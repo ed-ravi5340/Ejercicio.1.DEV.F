9e2bf5c1ceddfef6e41f6fad0d4c6c821fa2abe5
ed-ravi5340 <138498309+ed-ravi5340@users.noreply.github.com>
2023-09-18 21:03:34 -0600

    Add files via upload

--- ejercicio.js
+++ ejercicio.js
@@ -1,7 +1,21 @@
+//Ejercicio1.1//
+/*
 var dime = prompt('Dime, ¿Eres bellisimx?');
 if( dime == "si"){
     alert("Ciertamente!")
 }
 if( dime == "no"){
     alert('¡No te creo!')
+}
+*/
+
+//ejercicio1.2//
+var x = prompt('Dime un numero');
+if (x % 2 == 0)
+{
+  alert('¡Este numero es divisible entre 2!');
+}
+else
+{
+  alert('¡Este numero no es divisible entre 2!')
 }
