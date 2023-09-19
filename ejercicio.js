774dc2ea76a7f79ed8ec3986522a4ac715cf572d
ed-ravi5340 <138498309+ed-ravi5340@users.noreply.github.com>
2023-09-19 17:35:27 -0600

    Add files via upload

--- ejercicio.js
+++ ejercicio.js
@@ -1,4 +1,4 @@
-//Ejercicio1.1//
+//Ejercicio1.1: Solicitar al usuario que responda a la pregunta (“¿Eres bellisimo/a?”), en caso de contestar sí, responder “Ciertamente”, en caso de contestar no, responder: “No te creo”.//
 /*
 var dime = prompt('Dime, ¿Eres bellisimx?');
 if( dime == "si"){
@@ -9,7 +9,21 @@ if( dime == "no"){
 }
 */
 
-//ejercicio1.2//
+//ejercicio1.2: Solicitar al usuario un número, y determinar si es divisible entre dos o no. Mostrando al usuario un mensaje de “x número es divisible entre 2” o “x núm no es divisible entre 2”.//
+
+var x = prompt('Dime un numero');
+if (x % 2 == 0)
+{
+  console.log('es divisible entre 2!');
+}
+else
+{
+  console.log('no es divisible entre 2!')
+}
+
+
+//ejercicio1.3: Crear un programa que determine si un número introducido en un Prompt es par o no, la respuesta será mostrada en un Alert.//
+/*
 var x = prompt('Dime un numero');
 if (x % 2 == 0)
 {
@@ -19,3 +33,4 @@ else
 {
   alert('¡Este numero no es divisible entre 2!')
 }
+*/
